6e1b511d0761ffc0e333f6954e719b82e78981b4
komalarao <manoharvakamullu1999@gmail.com>
2022-11-18 19:52:20 -0800

    generating random number

---
+++ GeneratingRandomNumber.java
@@ -0,0 +1,11 @@
+package randomnumber;
+
+public class GeneratingRandomNumber {
+	public static void main(String[] args) {
+		var number = (Math.random());
+		System.out.println("generated number is "+number);
+		var randNumber = (Math.random()*10);
+		System.out.println("generated number is "+randNumber);
+	}
+
+}
